d53c7b7e5520e6f286e8a7e123efc467b0cb5a44
mkielar <mkielarv2@gmail.com>
2020-11-16 17:55:36 +0100

    reworked index.html to new UI

--- master/views.py
+++ master/views.py
@@ -1,9 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 
 def index(request):
-    return render(request, 'index.html')
+    offers = {
+        ("Lalka", "Bolesław Prus", 1890),
+        ("Zbrodnia i kara", "Fiodor Dostojewski", 1887),
+        ("Chłopi", "Władysław Reymont", 1904),
+        ("Krzyżacy", "Henryk Sienkiewicz", 1901),
+        ("Dziady część III", "Adam Mickiewicz", 1845),
+        ("Antygona", "Sofokles", 1993)
+    }
+
+    template = loader.get_template('index.html')
+    context = {
+        'offers': offers,
+    }
+    return HttpResponse(template.render(context, request))
+
+    # return render(request, 'index.html')
     # return HttpResponse("The D is silent.")
 
 
