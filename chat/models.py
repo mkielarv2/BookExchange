a18e35ea6e84063900b009c2756fa29faec0304f
SamPanDonte <bartolomeow@vp.pl>
2020-12-04 00:49:09 +0100

    changed message model, created comment model and serializer, implemented adding and viewing comments

--- chat/models.py
+++ chat/models.py
@@ -8,11 +8,38 @@ from offers.models import Offers
 class Messages(models.Model):
     offer = models.ForeignKey(Offers, on_delete=models.CASCADE)
     sender = models.ForeignKey(auth.User, on_delete=models.CASCADE)
-    message = models.TextField(null=False)
-    sending_time = models.DateTimeField(null=False)
+    recipient = models.ForeignKey(auth.User, on_delete=models.CASCADE, related_name='recipient')
+    message = models.TextField()
+    sending_time = models.DateTimeField()
 
 
 class MessagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Messages
         fields = '__all__'
+
+
+class MessagesDeserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Messages
+        fields = ('recipient', 'offer', 'message')
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(auth.User, on_delete=models.CASCADE)
+    author = models.ForeignKey(auth.User, on_delete=models.CASCADE, related_name='author')
+    content = models.TextField()
+    adding_time = models.DateTimeField()
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class CommentDeserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('user', 'content')
+
--- chat/views.py
+++ chat/views.py
@@ -1,44 +1,74 @@
+import json
+
 from django.contrib.auth.models import User
 from django.db.models import Q
-from django.http import JsonResponse, HttpResponseForbidden, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
+from django.utils.datetime_safe import datetime
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
 
-from chat.models import MessagesSerializer, Messages
+from chat.models import MessagesSerializer, Messages, MessagesDeserializer, Comment, CommentSerializer, \
+    CommentDeserializer
 from master.models import UserSerializer
 from offers.models import Offers
-
-
-class MessagesView(APIView):
-    """View for sending and receiving messages"""
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-    serializer = MessagesSerializer
-
-    def get(self, request, offer_id, sender_id=None):
-        """Get messages from chat"""
-        offer = Offers.objects.get(pk=offer_id)
-        if sender_id is None:
-            if offer.user == request.user:
-                return HttpResponseBadRequest()
-            sender_id = request.user.id
-        messages = Messages.objects.filter(offer__id=offer_id).order_by('sending_time')  # Get all messages to offer
-        messages = messages.filter(Q(sender__id=offer.user.id) | Q(sender__id=sender_id))  # Filter messages only from and to sender
-        serializer = self.serializer(messages, many=True)
-        return JsonResponse(serializer.data, safe=False)
 
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
-def get_chat_view(request, offer_id):
-    """View for author of the offer to get users that started"""
+def get_chat(request, offer_id):
+    """Get chats for offer"""
     offer = Offers.objects.get(pk=offer_id)
-    if offer.user != request.user:
-        return HttpResponseForbidden()
     user_ids = Messages.objects.values('sender__id').filter(offer=offer).filter(~Q(sender=request.user)).distinct()
     users = User.objects.filter(id__in=user_ids)
     serializer = UserSerializer(users, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def send_message(request):
+    """Send message"""
+    deserializer = MessagesDeserializer(data=json.loads(request.POST['payload']))
+    if not deserializer.is_valid():
+        return HttpResponseBadRequest(json.dumps(deserializer.errors))
+    message = deserializer.save(sender=request.user, sending_time=datetime.now())
+    if message.sender != message.offer.user and message.recipient != message.offer.user:
+        message.delete()
+        return HttpResponseBadRequest('One of users must be creator of the offer!')
+    return HttpResponse(status=200)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def get_messages(request, offer_id, user_id):
+    """Get messages from chat"""
+    user = request.user
+    messages = Messages.objects.filter(offer__id=offer_id).order_by('sending_time')  # Get all messages to offer
+    query = (Q(sender__id=user_id) & Q(recipient=user)) | (Q(recipient__id=user_id) & Q(sender=user))
+    messages = messages.filter(query)  # Filter messages only from and to user
+    serializer = MessagesSerializer(messages, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
+def get_comments(request, user_id):
+    """Get comments about user"""
+    comments = Comment.objects.filter(user__id=user_id)
+    serializer = CommentSerializer(comments, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def add_comment(request):
+    """Add comment about user"""
+    deserializer = CommentDeserializer(data=json.loads(request.POST['payload']))
+    if not deserializer.is_valid():
+        return HttpResponseBadRequest(json.dumps(deserializer.errors))
+    deserializer.save(author=request.user, adding_time=datetime.now())
+    return HttpResponse(status=200)
