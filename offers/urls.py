258123147912ed8e15b24650831346e4419711a3
SamPanDonte <bartolomeow@vp.pl>
2020-12-11 15:17:39 +0100

    added GET for localizations, categories, conditions

--- offers/urls.py
+++ offers/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'offers'
 
 urlpatterns = [
-
+    path('categories', views.get_categories, name='get_categories'),
+    path('localizations', views.get_localizations, name='get_localizations'),
+    path('conditions', views.get_conditions, name='get_conditions'),
 ]
--- offers/views.py
+++ offers/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from offers.models import Category, CategorySerializer, Localization, LocalizationSerializer, BookCondition, \
+    BookConditionSerializer
+
+
+def get_all_objects(model, serializer):
+    categories = model.objects.all()
+    serializer = serializer(categories, many=True)
+    return serializer.data
+
+
+@api_view(['GET'])
+def get_categories(request):
+    """Get all category object"""
+    return JsonResponse(get_all_objects(Category, CategorySerializer), safe=False)
+
+
+@api_view(['GET'])
+def get_localizations(request):
+    """Get all category object"""
+    return JsonResponse(get_all_objects(Localization, LocalizationSerializer), safe=False)
+
+
+@api_view(['GET'])
+def get_conditions(request):
+    """Get all category object"""
+    return JsonResponse(get_all_objects(BookCondition, BookConditionSerializer), safe=False)
+
