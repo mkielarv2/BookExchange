0000f22226a08e71e04b37fbdf0a642f6c804a0a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-12 23:55:26 +0100

    fix deployment

--- djangoBookExchange/settings.py
+++ djangoBookExchange/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 IMAGE_URL = 'static/Images/'
 
