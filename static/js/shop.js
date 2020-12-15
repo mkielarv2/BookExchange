9bf8dc34105993cef3bb451f4a8c386e4816a190
Emil Gutkiewicz <achinonek@gmail.com>
2020-12-16 00:21:57 +0100

    Update shop.js

--- static/js/shop.js
+++ static/js/shop.js
@@ -9,20 +9,20 @@ const Shop = {
             url: string,
             type: 'GET',
             success: function (data) {
-                $(".shop").html("")
                 for (let i = 0; i < data.length; i++) {
                     console.log(data[i])
                     let imgSrc = data[i].images[0] || "https://via.placeholder.com/600x500";
+                    let id = data[i].id;
                     let title = data[i].title;
                     let author = data[i].author;
-                    let condition = data[i].condition.condition;
-                    let category = data[i].category.name;
-                    let localization = data[i].location.name;
+                    let condition = data[i].condition;
+                    let category = data[i].category;
 
-                    let template = '<div data-id="product-id"><div class="product_image"><img src="' + imgSrc + '" alt=""></div><div class="product_info"><div>' + author + '</div><div>' + title + '</div><div>' + condition + '</div><div>' + category + '</div><div>' + localization + '</div></div>';
+                    let template = '<div data-data="' + id + '"><div class="product_image" ><img src="' + imgSrc + '" alt=""></div><div class="product_info"><div>' + author + '</div><div>' + title + '</div><div>' + condition + '</div><div>' + category + '</div><div>' + localization + '</div></div>';
 
-                    $(".shop").append(template)
+                    $(".shop").innerHTML += template;
                 }
+                LoadProductsOpeners();
             },
             error: function (data) {
             },
