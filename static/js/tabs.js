9af73b36552bf2732f5c12d6ceafce825ebce9c0
Emil Gutkiewicz <achinonek@gmail.com>
2020-12-15 16:35:06 +0100

    update front

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -1,39 +1,42 @@
 class Tab {
-  constructor(tab, func)
-  {
-    this.tab = tab;
-    this.htmlClass = 'tabIsShown';
-    this.closer = tab.querySelector('.closer');
+    constructor(tab, func) {
+        this.tab = tab;
+        this.htmlClass = 'tabIsShown';
+        this.closer = tab.querySelector('.closer');
 
-    this.closer.addEventListener('click', () => this.hide());
+        this.closer.addEventListener('click', () => this.hide());
 
-    if(typeof func === 'function')
-      this.func = func;
-    else
-      this.func = () => { };
-  }
+        if (typeof func === 'function')
+            this.func = func;
+        else
+            this.func = () => { };
+    }
 
-  show()
-  {
-    Nav.hide()
-    this.tab.classList.add(this.htmlClass);
-    this.func();
-  }
+    show(e) {
+        // jeśli istnieje event, to zbiera dane
+        if (typeof e === 'object') {
+            inboxid = e.target.dataset['data'];
 
-  hide()
-  {
-    this.tab.classList.remove(this.htmlClass);
-  }
+            console.log(inboxid);
+        }
+        Nav.hide()
+        this.tab.classList.add(this.htmlClass);
+        this.func();
+    }
+
+    hide() {
+        this.tab.classList.remove(this.htmlClass);
+    }
 
 }
 
 //funkcje wywołujące się po otwarciu któregoś tab'a
-var inbox_addListeners = undefined;
-const loadMessages = () => {
-  const Openers = document.querySelectorAll("[data-inbox]")
+var inboxid = undefined;
+const inbox_addListeners = () => {
+    const Openers = document.querySelectorAll("[data-data]")
 
-  for(const opener of Openers)
-    opener.addEventListener('click', () => SeeMessages.show())
+    for (const opener of Openers)
+        opener.addEventListener('click', (e) => SeeMessages.show(e))
 }
 //Zbieranie wszystkich okienek w jeden obiekt
 const Tabs = document.querySelectorAll('.tab');
@@ -43,7 +46,7 @@ const Sorting = new Tab(Tabs[0]);
 const Product = new Tab(Tabs[1]);
 const AddProduct = new Tab(Tabs[4]);
 const CheckProducts = new Tab(Tabs[5]);
-const CheckMessages = new Tab(Tabs[6], loadMessages);
+const CheckMessages = new Tab(Tabs[6], inbox_addListeners);
 const SeeMessages = new Tab(Tabs[7]);
 
 //EventListenery
@@ -51,5 +54,5 @@ document.querySelector("div.search_bar > div").addEventListener('click', () => S
 document.querySelector("div.shop").addEventListener('click', () => Product.show());
 document.querySelector("nav > div:nth-child(2) > div:nth-child(7) > div").addEventListener('click', () => AddProduct.show());
 document.querySelector("nav > div:nth-child(2) > div:nth-child(8) > div").addEventListener('click', () => CheckProducts.show());
-document.querySelector("nav > div:nth-child(2) > div:nth-child(9) > div").addEventListener('click', () => CheckMessages.show());
+document.querySelector("nav > div:nth-child(2) > div:nth-child(9) > div").addEventListener('click', (e) => CheckMessages.show());
 // document.querySelector(".product_messages_owner > .container > div > div:nth-child(2) > .product_actions > div").addEventListener('click', () => SeeMessages.show())
