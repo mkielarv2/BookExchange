93689cf71f896b67c645f31acc4849fd6f3b54b9
Emil Gutkiewicz <achinonek@gmail.com>
2020-12-15 23:24:48 +0100

    Update product.js

--- static/js/product.js
+++ static/js/product.js
@@ -1,25 +1,29 @@
+﻿
+const productLoad = (e) => {
+    const id = e.target.dataset['id'];
+    $.ajax({
+        url: '/offers/' + id + '/',
+        type: 'GET',
+        success: function (data) {
+            let title = data.title;
+            let author = data.author;
+            let condition = data.condition;
+            let category = data.category;
 
-const productLoad = () => {
-        const string = sortString();
+            let template = '<div class="photo">';
 
-        $.ajax({
-            url: string,
-            type: 'GET',
-            success: function (data) {
-                for (let i = 0; i < data.length; i++) {
-                    console.log(data[i])
-                    let imgSrc = data[i].images[0] || "https://via.placeholder.com/600x500";
-                    let title = data[i].title;
-                    let author = data[i].author;
-                    let condition = data[i].condition;
-                    let category = data[i].category;
+            let imgSrc;
+            while (data.images[j] != null) {
+                imgSrc = data.images[j];
+                template += '<img src="' + imgSrc + '" alt="dupsko">';
+            }
 
-                    let template = '<div data-id="product-id">< div class="product_image" ><img src="' + imgSrc + '" alt=""></div><div class="product_info"><div>' + author + '</div><div>' + title + '</div><div>' + condition + '</div><div>' + category + '</div><div>' + localization + '</div></div>';
+            template += '</div><div class="bar"><div>' + title + '</div><div>' + author + '</div></div><div class="info"><div>Wystawone przez: <span style="font-weight: bold">' + username + '</span></div><hr/><div><p>Opis wystawiającego</p><p>' + description + '</p></div></div>';
 
-                    $(".shop").innerHTML += template;
-                }
-            },
-            error: function (data) {
-            },
-        });
+            $(".shop").innerHTML += template;
+        },
+        error: function (data) {
+        },
+    });
 }
+
